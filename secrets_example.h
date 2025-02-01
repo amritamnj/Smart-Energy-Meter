782b34b3ddaa779599c80eb1b30c2bef5653d368
amritamnj <146801409+amritamnj@users.noreply.github.com>
2025-02-01 15:32:59 +0800

    Create secrets_example.h
    Before running the project, create a `secrets.h` file in the project directory using `secrets_example.h` as a template.

---
+++ secrets_example.h
@@ -0,0 +1,13 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+
+// Firebase credentials (Replace with your own)
+#define FIREBASE_HOST "https://your-firebase-db.firebaseio.com/"
+#define FIREBASE_AUTH "your-firebase-auth-token"
+
+// Blynk credentials (Replace with your own)
+#define BLYNK_AUTH "your-blynk-auth-token"
+#define WIFI_SSID "your-wifi-ssid"
+#define WIFI_PASS "your-wifi-password"
+
+#endif
